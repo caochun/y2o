dfdf0501f483db6ec546aeeb9979602119210c7c
Chun <caochun@gmail.com>
2025-04-20 17:57:16 +0800

    no luck

---
+++ o2a.sh
@@ -0,0 +1,1 @@
+atc --model=best.onnx --framework=5 --output=best --input_shape="images:1,3,640,640"  --soc_version=Ascend310P3 --insert_op_conf=aipp.cfg
--- yolo2onnx.py
+++ yolo2onnx.py
@@ -3,10 +3,13 @@ from ultralytics import YOLO
 # 加载已训练好的模型
 model = YOLO('best.pt')
 
-# 导出为 ONNX 格式，保持精度
-model.export(format='onnx', 
-             dynamic=True,     # 支持动态输入尺寸
-             simplify=False,    # 简化 ONNX 图结构
-             opset=12)         # 默认是 12，可按需要修改
+model.export(
+    format="onnx",
+    dynamic=False,
+    simplify=True,
+    opset=12,
+    nms=False,
+    imgsz=[640, 640]  # 固定图像大小
+)
 
 # 输出的文件将保存在相同目录下，名为 best.onnx
